7cbf0d02e69bd1d1f124f37f73e2ea10b02c30d1
Orlin <oritomov@yahoo.com>
2016-06-06 17:04:14 +0300

    Arduino twi/i2c interface

---
+++ Arduino/cdc/cdc.ino
@@ -0,0 +1,97 @@
+// CD Changer Interface for Audi Gamma CC Bose
+// by Orlin Tomov <mail:oritomov@yahoo.com>
+
+// Receives data as an I2C/TWI slave device
+
+// Created 5 Jun 2016
+
+// This code is in the public domain.
+
+
+#include <Wire.h>
+
+//const int ledPin = 13; // the pin that the LED is attached to
+
+const int cdc_on            = 0x21;
+const int cdc_off           = 0xA1;
+const int cdc_left_hold     = 0x01;
+const int cdc_left_release  = 0x81;
+const int cdc_right_hold    = 0x02;
+const int cdc_right_release = 0x82;
+const int cdc_down_hold     = 0x03;
+const int cdc_down_release  = 0x83;
+const int cdc_up_hold       = 0x04;
+const int cdc_up_release    = 0x84;
+
+void setup() {
+  // initialize the LED pin as an output:
+  //pinMode(ledPin, OUTPUT);
+
+  Wire.begin(64);               // join i2c bus with address #8
+  //Wire.setClock(9600);        // set baud rite
+ 
+  //digitalWrite(ledPin, HIGH); // turn on the LED:
+
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           // start serial for output
+}
+
+void loop() {
+  delay(100);
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+
+  //digitalWrite(ledPin, LOW); // if it's an L (ASCII 76) turn off the LED:
+
+  while (1 < Wire.available()) {  // loop through all but the last
+    char c = Wire.read();         // receive byte as a character
+    //Serial.print(c);            // print the character
+    int x = Wire.read();          // receive byte as an integer
+    switch (x) {
+      case cdc_on:
+        Serial.println("ON");
+// TODO: answer
+//        Wire.write(byte(0x34));
+//        Wire.write(byte(0xBE));
+//        Wire.write(byte(0xFF));
+//        Wire.write(byte(0xFF));
+//        Wire.write(byte(0xFF));
+//        Wire.write(byte(0xFF));
+//        Wire.write(byte(0xCF));
+//        Wire.write(byte(0x3C));
+        break;
+      case cdc_off:
+        Serial.println("OFF");
+        break;
+      case cdc_left_hold:
+        Serial.println("LEFT HOLD");
+        break;
+      case cdc_left_release:
+        Serial.println("LEFT RELEASE");
+        break;
+      case cdc_right_hold:
+        Serial.println("RIGHT HOLD");
+        break;
+      case cdc_right_release:
+        Serial.println("RIGHT RELEASE");
+        break;
+      case cdc_down_hold:
+        Serial.println("DOWN HOLD");
+        break;
+      case cdc_down_release:
+        Serial.println("DOWN RELEASE");
+        break;
+      case cdc_up_hold:
+        Serial.println("UP HOLD");
+        break;
+      case cdc_up_release:
+        Serial.println("UP RELEASE");
+        break;
+      default:
+        Serial.println(x);        // print the integer
+    }
+  }
+}
