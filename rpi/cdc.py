f10d35acca706b0f1ca17777500ae3f30ce6f390
Orlin <oritomov@yahoo.com>
2016-06-14 13:55:18 +0300

    more func

--- rpi/cdc.py
+++ rpi/cdc.py
@@ -12,20 +12,6 @@ DEV_SD_STAR = "/dev/sd*"
 USB_PATH = "/mnt/usb"
 CDC_PATH = USB_PATH + "/cdc"
 
-on=True
-device=None
-tracks=None
-player=None
-trackNum=0
-
-def mount(source, target, fs, options=''):
-	ret = ctypes.CDLL('libc.so.6', use_errno=True).mount(source, target, fs, 0, options)
-	if ret < 0:
-		errno = ctypes.get_errno()
-		#raise RuntimeError
-		print("Error mounting {} ({}) on {} with options '{}': {}".
-			format(source, fs, target, options, os.strerror(errno)))
-
 # check devices for a class id
 def find_dev(dev_class_id):
 	device = usb.core.find()
@@ -48,55 +34,101 @@ def find_dev(dev_class_id):
 	# found nothing
 	return False
 
-# read cmds from the radio and act like a cd changer
+def mount():
+	sd = glob.glob(DEV_SD_STAR)
+	source = sd[0]
+	target = USB_PATH
+	fs = "vfat"
+	options = ''
+	ret = ctypes.CDLL('libc.so.6', use_errno=True).mount(source, target, fs, 0, options)
+	if ret < 0:
+		errno = ctypes.get_errno()
+		#raise RuntimeError
+		print("Error mounting {} ({}) on {} with options '{}': {}".
+			format(source, fs, target, options, os.strerror(errno)))
+
+on=True
+device=None
+tracks=None
+player=None
+albumNum=0
+trackNum=0
+
+# read cmds from the hu and act like a cd changer
 while True:
-#if True:
-	#try:
+	try:
+
+		# find a device
+		if (device is None) and find_dev(MASS_STORAGE):
+			device = MASS_STORAGE
+			print "found Mass Storage"
+
+			if not os.path.exists(CDC_PATH):
+				mount()
+				tracks = None
+
+		# tracks list
+		if (device is not None) and (tracks is None):
+			albums = os.walk(DIR_PATH).next()[1]
+			if albumNum >= len(albums)
+				albumNum = 0
+				trackNum = 0
+			if albumNum < 0
+				albumNum = len(albums) - 1
+				trackNum = 0
+			album = albums[albumNum]
+			tracks = glob.glob(CDC_PATH + "/" + album + "/*.mp3")
+			# TODO randomize
+			player = None
+
+		if (tracks is not None) and (trackNum >= len(tracks))
+			# next album
+			albumNum = albumNum + 1
+			trackNum = 0
+			tracks = None
+
+		if (tracks is not None) and (trackNum < 0)
+			#prev album
+			albumNum = albumNum - 1
+			trackNum = 0
+			tracks = None
 
 		# start play
-		if on and player is None:
-
-			# find a device
-			if (device is None) and find_dev(MASS_STORAGE):
-				device = MASS_STORAGE
-				print "found Mass Storage"
-
-				if not os.path.exists(CDC_PATH):
-					sd = glob.glob(DEV_SD_STAR)
-					mount(sd[0], USB_PATH, "vfat")
-					print "muont Mass Storage"
-
-			# tracks list
-			if (device is not None) and (tracks is None):
-				tracks = glob.glob(CDC_PATH + "/*.mp3")
-				print "found ", len(tracks), "tracks"
-
-			# play
-			if (tracks is not None) and (player is None):
-				# play
-				player = subprocess.Popen(["omxplayer",tracks[trackNum]],stdin=subprocess.PIPE) #,stdout=subprocess.PIPE,stderr=subprocess.PIPE
-				print "play " + tracks[trackNum]
+		if on and (tracks is not None) and (player is None):
+			player = subprocess.Popen(["omxplayer",tracks[trackNum]],stdin=subprocess.PIPE) #,stdout=subprocess.PIPE,stderr=subprocess.PIPE
 
 		#check playing
-		if (player is not None):
+		if player is not None:
 			fi = player.poll()
-			#print fi
 			if fi is not None:
 				player = None
 				# next track
 				trackCount = len(tracks)
 				trackNum = trackNum + 1
-				if trackNum > trackCount - 1:
-					trackNum = 0
-				print "start ", tracks[trackNum]
+
+		# next
+		if next and (player is not None):
+			next = None
+			player.stdin.write("q")
+			player = None
+			trackNo = trackNo + 1
+
+		# prev
+		if prev and (player is not None):
+			prev = None
+			player.stdin.write("q")
+			player = None
+			trackNo = trackNo - 1
 
 		# stop play
-		if not on and player is not None:
+		if not on and (player is not None):
 			player.stdin.write("q") # test stop
 			player = None
 
-		#print "wait"
 		sleep(0.1)
 
-	#except:
-	#	raise
+	except:
+		device=None
+		tracks=None
+		player=None
+		raise
